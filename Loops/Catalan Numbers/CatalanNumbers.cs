70c24e54ce680ce6a8265f168b343034d0b8084b
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-02-28 11:57:14 +0200

    Loops up to task 8 included.

---
+++ Loops/Catalan Numbers/CatalanNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+class CatalanNumbers
+{
+    static void Main()
+    {
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        BigInteger factorial = 1;
+        BigInteger doublefactorial = 1;
+
+        for (int i = 1, j = 1; i <= (2 * userInput); i++)
+        {
+            doublefactorial *= i;
+
+            while (j <= userInput)
+            {
+                factorial *= j;
+                j++;
+            }
+        }
+
+        BigInteger result = doublefactorial / (factorial * factorial * (userInput + 1));
+
+        Console.WriteLine(result);
+    }
+}
+
