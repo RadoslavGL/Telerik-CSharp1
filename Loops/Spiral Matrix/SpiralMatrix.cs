2fb48e9764ec7d8a4360306fbdda4e5d1da034f6
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-03-03 15:23:52 +0200

    pause at spiral matrix

---
+++ Loops/Spiral Matrix/SpiralMatrix.cs
@@ -0,0 +1,30 @@
+﻿//Write a program that reads from the console a positive integer number N (1 ≤ N ≤ 20) and 
+//prints a matrix holding the numbers from 1 to N*N in the form of square spiral like in the examples below.
+
+// here is an illustration http://www.crazyforcode.com/print-square-matrix-spiral-form/
+
+
+namespace Spiral_Matrix
+{
+    using System;
+    class SpiralMatrix
+    {
+        static void Main()
+        {
+            int sideLength = int.Parse(Console.ReadLine());
+            int value = 1;
+
+            for (int i = 1; i <= sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    Console.Write("{0} ", value);
+                    value++;
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
